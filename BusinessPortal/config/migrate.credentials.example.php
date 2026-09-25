db00a09369843dc40c6ebc67e3b14e321e81e09f
Ahmad Alkhatib <ahmadalkhatibwork@yahoo.com>
2026-09-25 12:42:52 +0300

    Update migration credentials example with a new key

--- BusinessPortal/config/migrate.credentials.example.php
+++ BusinessPortal/config/migrate.credentials.example.php
@@ -5,5 +5,5 @@
  * Use a long random value (at least 16 characters), e.g. from: php -r "echo bin2hex(random_bytes(24));"
  */
 return [
-    'key' => 'CHANGE_ME_TO_A_LONG_RANDOM_STRING',
+    'key' => '6dffe54a2b56d3068f8820c03ed60c90b7efd6a8',
 ];
